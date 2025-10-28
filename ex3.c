dee85a810281f1ef1e5226cae2983b638e09590a
TouristOmer <omerfh025@gmail.com>
2025-10-29 00:00:21 +0300

    Update ex3.c
    Ömer Faruk Han
    250543022

--- ex3.c
+++ ex3.c
@@ -1,2 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main() {
+    char pet[10];
+    printf("Do you have cat or dog?\n");
+    scanf("%s", pet);
+    
+    if (strcmp(pet, "cat") == 0) {
+        printf("Meow!\n");
+    }
+    
+    else if (strcmp(pet, "dog") == 0){
+        printf("Woof!\n");
+    }
+    
+    else {
+        printf("You must be enter cat or dog.");
+    }
+    
+    return 0;
+}
 
