688d545c17e1c7ab8d3b4a5b6e01bf6c7979d21c
TouristOmer <omerfh025@gmail.com>
2025-10-28 23:03:21 +0300

    Update ex2.c
    Ömer Faruk Han
    250543022

--- ex2.c
+++ ex2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+
+    int age;
+    printf("How old are you?\n");
+    scanf("%d", &age);
+
+    for(int i = 0; i < age; i++){
+
+        printf("Happy birthday!\n");
+    }
+    return 0;
+}
