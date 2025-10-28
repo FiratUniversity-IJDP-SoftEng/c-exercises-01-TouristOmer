ca4b43ef7bd922cd92e4d229516a8a167c5907f1
TouristOmer <omerfh025@gmail.com>
2025-10-29 00:32:23 +0300

    Update ex5.c
    Ömer Faruk Han
    250543022

--- ex5.c
+++ ex5.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void sayHello() {
+    
+    char name[20];
+    
+    printf("What's your name?\n");
+    scanf("%s", name);
+    printf("Hello %s!", name);
+    
+}
+
+int main(){
+    sayHello();
+    
+    return 0;
+}
