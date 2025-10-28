b952e206fcdf1b59a187d5226f8f57366a82d984
TouristOmer <omerfh025@gmail.com>
2025-10-28 23:14:21 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+
+    int number;
+    printf("Guess the secret number (1-5): ");
+    scanf("%d", &number);
+
+    if (number == 3) {
+
+    printf("Correct!\n");
+}
+    else if (number >= 1 && number <= 5) {
+
+    printf("Wrong!\n");
+}
+    else {
+
+    printf("Your guess must be between 1-5.\n");
+}
+
+    return 0;
+}
